30ade3f758cc9af2c59796f8cd14104095f7ca49
bertyn99 <bboulikou@gmail.com>
2022-07-08 10:58:18 +0200

    ajout googeInput

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,17 +2,20 @@ import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
 import tw from "../utils/tailwind";
 import NavOptions from "../components/NavOptions";
+import GooglePlacesInput from "../components/GooglePlacesInput";
 
 type Props = {};
 
 const HomeScreen = (props: Props) => {
   return (
-    <SafeAreaView style={tw`h-full  `}>
+    <SafeAreaView style={tw`h-full`}>
       <View style={tw`p-5`}>
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
         />
+        <GooglePlacesInput />
+
         <NavOptions></NavOptions>
       </View>
     </SafeAreaView>
